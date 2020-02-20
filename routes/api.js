490b299d781c70709c5514945c0d5894d7639580
haksonglee <blank@nate.com>
2020-02-20 16:54:28 +0900

    111

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('api with a resource');
+});
+
+router.get('/say', function(req, res, next) {
+  res.send('say with a resource');
+});
+
+router.get('/sayHello', function(req, res, next) {
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: "hello I'm Ryan"
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+module.exports = router;
