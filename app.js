9f4b65df2a27dd4a9c387ede43fa9d83e00dbe41
haksonglee <blank@nate.com>
2020-02-20 14:45:55 +0900

    heroku ready

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello huhu haksong  aaa")
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var http = require('http');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('express server starting...' + app.get('port'));
+});
