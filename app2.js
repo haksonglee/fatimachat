a08fc5164cdec254f2cc0ddae7b0174cbbec742e
haksonglee <blank@nate.com>
2020-02-21 11:53:21 +0900

    1

--- app2.js
+++ app2.js
@@ -3,7 +3,7 @@ const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const apiRouter = express.Router();
+const router = express.Router();
 
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
@@ -11,44 +11,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use('/api', apiRouter);
-
-apiRouter.post('/sayHello', function(req, res) {
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText: {
-            text: "hello I'm Ryan"
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
+router.route('/sayHello').post(function(req, res) {
+  console.log("hhhhhh");
 });
 
-apiRouter.post('/showHello', function(req, res) {
-  console.log(req.body);
-
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleImage: {
-            imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-            altText: "hello I'm Ryan"
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
-});
+app.use('/', router);
 
 app.listen(3000, function() {
   console.log('Example skill server listening on port 3000!');
