a6d79a69c0f9c6454becf06fdb520ad8f797a4fc
haksonglee <blank@nate.com>
2020-02-24 17:29:53 +0900

    1

--- routes/listcard.js
+++ routes/listcard.js
@@ -10,24 +10,28 @@ router.post('/listcard', function(req, res) {
       {
         listCard: {
           header: {
-            title: "카카오 i 디벨로퍼스를 소개합니다",
-            imageUrl: "http://k.kakaocdn.net/dn/xsBdT/btqqIzbK4Hc/F39JI8XNVDMP9jPvoVdxl1/2x1.jpg"
+            title: "창원파티마병원 소화기내과",
+            description: "창원파티마병원 소화기내과 의료진소개입니다",
+            //imageUrl: "http://k.kakaocdn.net/dn/xsBdT/btqqIzbK4Hc/F39JI8XNVDMP9jPvoVdxl1/2x1.jpg",
+            link: {
+              web: "https://www.fatimahosp.co.kr/pages/department?deptdoctor=MA"
+            }
           },
           items: [
             {
-              title: "Kakao i Developers",
-              description: "새로운 AI의 내일과 일상의 변화",
-              imageUrl: "http://k.kakaocdn.net/dn/APR96/btqqH7zLanY/kD5mIPX7TdD2NAxgP29cC0/1x1.jpg",
+              title: "박종호",
+              description: "위, 식도, 대장, 간, 췌담도",
+              imageUrl: "http://www.fatimahosp.co.kr/uploads/clinicimg/20100824070913_2.jpg",
               link: {
-              web: "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
+              web: "https://www.fatimahosp.co.kr/pages/department?drcode=02127"
               }
             }
           ],
           buttons: [
             {
-              label: "구경가기",
+              label: "모바일 홈페이지",
               action: "webLink",
-              webLinkUrl: "https://namu.wiki/w/%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88"
+              webLinkUrl: "https://www.fatimahosp.co.kr/pages/department?deptdoctor=MA"
             }
           ]
         }
