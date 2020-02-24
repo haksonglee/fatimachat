fbdd1d82b2256fcc09de7e7805fc8b006b8fc943
haksonglee <blank@nate.com>
2020-02-24 17:39:37 +0900

    1

--- routes/listcard.js
+++ routes/listcard.js
@@ -25,7 +25,40 @@ router.post('/listcard', function(req, res) {
               link: {
               web: "https://www.fatimahosp.co.kr/pages/department?drcode=02127"
               }
-            }
+            },
+            {
+              title: "이주용",
+              description: "상하부위장관, 간질환, 췌담도",
+              imageUrl: "https://www.fatimahosp.co.kr/uploads/clinicimg/20100824071828_5.jpg",
+              link: {
+              web: "https://www.fatimahosp.co.kr/pages/department?drcode=02727"
+              }
+            },
+            {
+              title: "신재욱",
+              description: "위, 대장, 췌담도",
+              imageUrl: "https://www.fatimahosp.co.kr/uploads/clinicimg/20120302090906_8.jpg",
+              link: {
+              web: "https://www.fatimahosp.co.kr/pages/department?drcode=03127"
+              }
+            },
+            {
+              title: "진수신",
+              description: "위, 식도, 대장, 간, 췌담도",
+              imageUrl: "https://www.fatimahosp.co.kr/uploads/clinicimg/20150309054345_소화기내과 진수진과장.jpg",
+              link: {
+              web: "https://www.fatimahosp.co.kr/pages/department?drcode=03420"
+              }
+            },
+            {
+              title: "김완철",
+              description: "위, 식도, 대장, 간, 췌담도",
+              imageUrl: "https://www.fatimahosp.co.kr/uploads/clinicimg/04201_f473b54943f0d08928de267f7f6c2c3c.png",
+              link: {
+              web: "https://www.fatimahosp.co.kr/pages/department?drcode=04201"
+              }
+            },
+
           ],
           buttons: [
             {
