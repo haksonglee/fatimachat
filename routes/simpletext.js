5ba95492460178bd59043fef78ceb5e34300b9ea
haksonglee <blank@nate.com>
2020-02-26 18:46:56 +0900

    1

--- routes/simpletext.js
+++ routes/simpletext.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 //localhost:3000/simpletext/test
 router.post('/test', function(req, res) {
-  var name = req.body.action.drname;
+  var name = req.body.action.name;
   console.log(name)
   const responseBody = {
     version: "2.0",
