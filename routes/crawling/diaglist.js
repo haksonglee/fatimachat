6fb05f22b42298585fe8c0047f433c82be37f6ba
haksonglee <blank@nate.com>
2020-02-26 09:49:21 +0900

    1

---
+++ routes/crawling/diaglist.js
@@ -0,0 +1,98 @@
+//drlist.json생성 플로어
+
+const express = require("express");
+const router = express.Router();
+//const request = require("request");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+var fs=require('fs')
+
+//var dept;
+//dept = 'PD'
+
+//const getDrlist = require(__dirname + '/drlist.js')
+const dataPath = __dirname + '/deptlist.json'
+const dataPath2= __dirname + '/diaglist.json'
+var dataArr=[];
+
+//console.log(dataPath)
+var fs=require('fs')
+
+async function getDrlist(dept) {  //console.log("nodecraw start");
+
+  //console.log(dept)
+  const getHtml = async () => {
+    try {
+      return await axios.get("https://www.fatimahosp.co.kr/pages/department", {
+        params: {
+          deptdoctor: dept
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //https://www.fatimahosp.co.kr/pages/department?deptdoctor=MA
+
+  getHtml().then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("div.doctor_list ul").children("li.col-md-6");
+
+    $bodyList.each(function(i, elem) {
+      var datai = {
+        web: "https://www.fatimahosp.co.kr/pages/department?drcode=" +
+                $(this)
+                .find("footer.schedule_calendar")
+                .attr("data-drcode")
+      };
+
+      var data = {
+
+        description: $(this)
+          .find("div.card-content-textarea dl dd")
+          .text()
+      };
+
+      dataArr.push(data);
+      fs.writeFileSync(dataPath2, JSON.stringify(dataArr), function(error, data){
+        if (error) {throw error};
+      });
+
+    });
+
+
+    //console.log(dataArr)
+    //const data = ulList.filter(n => n.name);
+    //return data;
+  });// .then(res => console.log(res));
+  //return ulList;
+  //console.log(str)
+  //return 'testreturn'
+  //return str;
+};
+
+var string= fs.readFileSync(dataPath, 'utf-8');
+//console.log(JSON.parse(string))
+var data=JSON.parse(string)
+var body=[];
+//console.log(data.length)
+for (var i=0;i<data.length;i++){
+  var item = data[i];
+  //body.push(item.deptcode)
+  //if(item.deptcode == 'GS') {
+    getDrlist(item.deptcode);
+  //}
+};
+
+
+  //console.log('end dataArr')
+//console.log(body)
+//fs.appendFileSync(dataPath, JSON.stringify(dataArr), function(error, data){
+  //if (error) {throw error};
+//})
+
+//module.exports = getDrlist ;
---
+++ routes/listcard_drsearch.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+
+//크롤링
+
+const getDrlist = require(__dirname + '/crawling/drlist.js')
+const dataPath=__dirname + '/crawling/drlist.json'
+var fs=require('fs')
+
+//http://localhost:3000/api/listcard_drlist/
+var dept = "";
+var drname=  "";
+
+router.post('/', function(req, res) {
+  var params = req.body.action.params
+  //console.log(params['진료과명'])
+  //dept = params['dept']
+  drname = params['진료의사'] //시나리오 필수파라미터 이름 동일해야함
+
+  //dept = "ET"
+  //deptname = "이비인후과"
+  //getDrlist(dept);
+
+  var string= fs.readFileSync(dataPath, 'utf-8');
+  var data=JSON.parse(string)
+  var body=[];
+  //console.log(data.length)
+
+  for (var i=0;i<data.length;i++){
+     var item = data[i];
+     if(item.title == drname) {
+       dept = item.dept
+       body.push(item)
+     }
+
+  };
+
+  const responseBody = {
+  version: "2.0",
+  template: {
+    outputs: [
+      {
+        listCard: {
+          header: {
+            title: "창원파티마병원 " + deptname,
+            imageUrl: "" //"https://www.fatimahosp.co.kr/pages/department?deptdoctor="+ dept
+          },
+          items:
+            body
+          ,
+          buttons: [
+            {
+              label: "모바일 홈페이지",
+              action: "webLink",
+              webLinkUrl: "https://www.fatimahosp.co.kr/pages/department?deptdoctor=" + dept
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
+  res.status(200).send(responseBody);
+
+});
+
+module.exports = router;
