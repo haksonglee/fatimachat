6956ecf4e0663584cc2a08c4a1c8aec867adfe17
haksonglee <blank@nate.com>
2020-02-27 10:27:50 +0900

    1

--- routes/simpletext.js
+++ routes/simpletext.js
@@ -7,8 +7,11 @@ router.post('/test', function(req, res) {
   var name = req.body.action.name;
   //var name = req.body.contexts.params."의사명".value;
   console.log(name)
-  var name2 = req.body.contexts.[0]  ;
+  var name2 = req.body.contexts;
   console.log(JSON.stringify(name2))
+
+  var name3 = req.body.contexts[0].params."진료과".value;
+  console.log(name3)
   //var reqjson = JSON.stringify(req.body)
 
 
