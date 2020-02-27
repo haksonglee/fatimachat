b6274b83d60d1b62815332c4b3120e04ac2c7099
haksonglee <blank@nate.com>
2020-02-28 08:38:35 +0900

    1

--- routes/card_drlist.js
+++ routes/card_drlist.js
@@ -44,48 +44,7 @@ router.post('/', function(req, res) {
       {
         "carousel": {
           "type": "basicCard",
-          "header": {
-            "title": deptname + "의료진 입니다"
-          },
           "items": [
-            {
-              "title": "보물상자",
-              "description": "보물상자 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
-              "buttons": [
-                {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
-                  "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                }
-              ]
-            },
-            {
-              "title": "보물상자2",
-              "description": "보물상자2 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
-              "buttons": [
-                {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
-                  "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                }
-              ]
-            },
             {
               "title": "보물상자3",
               "description": "보물상자3 안에는 뭐가 있을까",
@@ -104,7 +63,7 @@ router.post('/', function(req, res) {
                   "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
                 }
               ]
-            }
+            }            
           ]
         }
       }
