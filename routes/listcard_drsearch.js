7f7b37f325cfff39f9b943bf1599c490032ff32f
haksonglee <blank@nate.com>
2020-02-27 16:10:50 +0900

    111

--- routes/listcard_drsearch.js
+++ routes/listcard_drsearch.js
@@ -14,6 +14,9 @@ router.post('/', function(req, res) {
   var params = req.body.action.params
   console.log(params['진료의사'])
 
+  var name2 = req.body;
+  console.log(JSON.stringify(name2))
+
   //dept = params['dept']
   drname = params['진료의사'] //시나리오 필수파라미터 이름 동일해야함
 
