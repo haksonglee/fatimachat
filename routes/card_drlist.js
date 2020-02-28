6052fa051ab87fc7e7280a42c31cb7033ca5a2ed
haksonglee <blank@nate.com>
2020-02-28 09:08:59 +0900

    1

--- routes/card_drlist.js
+++ routes/card_drlist.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 //크롤링
 
 const getDrlist = require(__dirname + '/crawling/drlist.js')
-const dataPath=__dirname + '/crawling/drlist.json'
+const dataPath=__dirname + '/crawling/drlist_card.json'
 var fs=require('fs')
 
 //http://localhost:3000/api/listcard_drlist/
@@ -45,44 +45,7 @@ router.post('/', function(req, res) {
         "carousel": {
           "type": "basicCard",
           "items": [
-            {
-              "title": "보물상자3",
-              "description": "보물상자3 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
-              "buttons": [
-                {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
-                  "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                }
-              ]
-            },
-            {
-              "title": "보물상자2",
-              "description": "보물상자2 안에는 뭐가 있을까",
-              "thumbnail": {
-                "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-              },
-              "buttons": [
-                {
-                  "action": "message",
-                  "label": "열어보기",
-                  "messageText": "짜잔! 우리가 찾던 보물입니다"
-                },
-                {
-                  "action":  "webLink",
-                  "label": "구경하기",
-                  "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                }
-              ]
-            }            
+            body
           ]
         }
       }
