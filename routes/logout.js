40457035c76a4779dd8fe2a9437971e453636d31
haksonglee <blank@nate.com>
2020-02-28 18:24:33 +0900

    111

--- routes/logout.js
+++ routes/logout.js
@@ -1,67 +1,30 @@
+//로그아웃
+
 const router = require('express').Router();
-
-//크롤링
-
-//const getDrlist = require(__dirname + '/crawling/drlist.js')
-//const dataPath=__dirname + '/crawling/drlist.json'
-//var fs=require('fs')
-
-//http://localhost:3000/api/listcard_drlist/
-var dept = "";
-var deptname=  "";
-
 router.post('/', function(req, res) {
-  //var params = req.body.action.params
-  //var patient_name = params['patient_name']
-  //var patient_birth = params['patient_birth']
-  //try {
-  //      var patient_hospno = req.body.contexts[0].params.patient_confirm.value;
-  //    } catch (err) {
-    //console.error(err)
-  //      patient_hospno = "undefined"
-  //      }
 
-   //dept = params['dept']
-  //console.log(JSON.stringify(params))
-  //deptname = params['진료과명'] //시나리오 필수파라미터 이름 동일해야함
-  //console.log("patient_name : " + patient_name)
-  //console.log("patient_birth : " + patient_birth)
-  //console.log("patient_confirm : " + patient_hospno)
-  //dept = "ET"
-  //deptname = "이비인후과"
-  //getDrlist(dept);
-  //var name2 = req.body;
-  //console.log(JSON.stringify(name2))
-  //var name2 = req.body;
-  //console.log(JSON.stringify(name2))
-  //console.log('pass login module')
-
-  //break;
-
-  //var string= fs.readFileSync(dataPath, 'utf-8');
-  //var data=JSON.parse(string)
-  var body=[];
-  var responseBody = {
-      "version": "2.0",
-      "template": {
-        "outputs": [
-            {
-                "simpleText": {
-                    "text": "로그아웃 되었습니다."
-                }
-            }
+var body=[];
+var responseBody = {
+    "version": "2.0",
+    "template": {
+      "outputs": [
+          {
+              "simpleText": {
+                  "text": "로그아웃 되었습니다."
+              }
+          }
+      ]
+    },
+    "context": {
+        "values":[
+          {
+            "name":"patient_info",
+            "lifeSpan":0
+          }
         ]
-      },
-      "context": {
-          "values":[
-            {
-              "name":"patient_info",
-              "lifeSpan":0
-            }
-          ]
-        }
       }
-    res.status(200).send(responseBody);
+    }
+  res.status(200).send(responseBody);
 });
 
 module.exports = router;
--- routes/quickreplies.js
+++ routes/quickreplies.js
@@ -1,27 +1,27 @@
-// quickreplies.js
+// quickreplies.js 진료과 리스트
 
 const router = require('express').Router();
 
 router.post('/quickReplies', function(req, res) {
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText:{
-            text:"hello"
-          }
+const responseBody = {
+  version: "2.0",
+  template: {
+    outputs: [
+      {
+        simpleText:{
+          text:"hello"
         }
-      ],
-      quickReplies: [
-        {
-          label: "처음으로",
-          action:"message",
-          messageText: "처음으로"
-        }
+      }
+    ],
+    quickReplies: [
+      {
+        label: "처음으로",
+        action:"message",
+        messageText: "처음으로"
+      }
     ]
   }
-};
+}
   res.status(200).send(responseBody);
 });
 
