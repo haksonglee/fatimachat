e2b4eafdb3588085654260a247cfdfbe48d7a72a
haksonglee <blank@nate.com>
2020-03-05 13:59:54 +0900

    1

--- routes/listcard_drinfo.js
+++ routes/listcard_drinfo.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
-var deptname = "";
-var drname = "";
+var deptname;
+var drname;
 
 router.post('/', function(req, res) {
   var params = req.body.action.params
   deptname = params['진료과명'] //시나리오 필수파라미터 이름 동일해야함
-
+  drname
   var drlist_script = require('./call_drlist');
   var drlist_bodydata = JSON.stringify(drlist_script.call_drlist(deptname, drname, 'dept'))
 
