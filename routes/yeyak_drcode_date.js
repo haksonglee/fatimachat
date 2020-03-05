336e60d8f83f288b62af3fae244b31b30b9f6ea6
haksonglee <blank@nate.com>
2020-03-05 12:32:31 +0900

    1

--- routes/yeyak_drcode_date.js
+++ routes/yeyak_drcode_date.js
@@ -81,7 +81,7 @@ router.post('/', function(req, res) {
       }*/
 
       var drlist_script = require('./call_drlist');
-      var drlist_bodydata1 = drlist_script.call_drlist(deptname, drname, 'dept')
+      var drlist_bodydata1 = JSON.parse(drlist_script.call_drlist(deptname, drname, 'dept'))
       drlist_bodydata1.link.web = drlist_bodydata1.link.web +  '&patient_name='+ patient_name + '&patient_birth=' + patient_birth + '&deptname=' + deptname + '&drname=' + drname + '&yeyakdate=' + yeyakdate
       console.log("check link  = " + drlist_bodydata1)
       console.log("check link pamameter = " + drlist_bodydata1.link.web)
