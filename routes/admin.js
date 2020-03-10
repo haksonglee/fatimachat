527da55244a972b9631e1533d50e11a0726792c8
haksonglee <blank@nate.com>
2020-03-10 12:07:24 +0900

    1

--- routes/admin.js
+++ routes/admin.js
@@ -4,19 +4,19 @@ var exex_script1 = require('./crawling/reservation_department')
 var exex_script2 = require('./crawling/department')
 var exex_script3 = require('./crawling/drlist')
 
-router.get('/reservation_department', async function(req, res) {
+router.post('/reservation_department', async function(req, res) {
 
   exex_script1.reservation_department()
   res.send({result:"ok"})
 }); // router
 
-router.get('/department', async function(req, res) {
+router.post('/department', async function(req, res) {
 
   exex_script2.department()
   res.send({result:"ok"})
 }); // router
 
-router.get('/drlist', async function(req, res) {
+router.post('/drlist', async function(req, res) {
 
   exex_script3.drlist()
   res.send({result:"ok"})
