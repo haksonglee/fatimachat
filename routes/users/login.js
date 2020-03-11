21f3da4812daefe2f4f7750eb35bd6677875ce0f
haksonglee <blank@nate.com>
2020-03-11 11:00:31 +0900

    1

--- routes/users/login.js
+++ routes/users/login.js
@@ -6,7 +6,7 @@ var responseBody;
 
 router.post('/', async function(req, res) {
 
-  //console.log(req.body.bot)
+  console.log(JSONG.stringfy(req.body.action.params))
   var botids = req.body.bot
   var params = req.body.action.params
   var botid = botids['id']
