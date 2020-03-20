63f93c6dd68981a388048cace17aa2f886e27f32
haksonglee <blank@nate.com>
2020-03-20 15:12:05 +0900

    11

--- routes/call_yedate.js
+++ routes/call_yedate.js
@@ -19,9 +19,10 @@ exports.call_yedate = function(deptname, drname) {
 
 //console.log(buttonstr1)
 buttonstr1 = JSON.parse(buttonstr1)
+let dept = filterbody[0].dept
 
 let texthelp = '';
-let yelink = filterbody[0].link.web
+//let yelink = filterbody[0].link.web
 //console.log(typeof filterbody, filterbody.bodydeptname)
 //console.log('deptname = ' + deptname)
 switch (deptname) {
@@ -43,7 +44,7 @@ switch (deptname) {
     buttonstr2 = {
       label: "모바일예약 이동",
       action: "webLink",
-      webLinkUrl: yelink
+      webLinkUrl: "https://www.fatimahosp.co.kr/pages/department?deptdoctor=" + dept
     }
     //texthelp = "진료를 원하시는 의료진을 선택해주세요."
 }
@@ -69,7 +70,7 @@ const responseBody = {
       {
         simpleText: {
           text: texthelp
-        } 
+        }
       }
     ]
   }
