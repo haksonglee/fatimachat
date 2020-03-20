81cf91e67cf171882e767c76c2d68e8b64980068
haksonglee <blank@nate.com>
2020-03-20 16:09:47 +0900

    111

--- routes/call_yedate.js
+++ routes/call_yedate.js
@@ -17,65 +17,67 @@ exports.call_yedate = function(deptname, drname) {
         "action": "message",
         "messageText": "${deptname} 예약" }`
 
-//console.log(buttonstr1)
-buttonstr1 = JSON.parse(buttonstr1)
-filterbody[0].title = filterbody[0].title + ' ' + filterbody[0].deptname
-let drcode = filterbody[0].drcode
-
-let texthelp = '';
-let yelink = "https://www.fatimahosp.co.kr/pages/department?drcode=" + drcode
-//console.log(typeof filterbody, filterbody.bodydeptname)
-//console.log('deptname = ' + deptname)
-switch (deptname) {
-  case '피부과':
-  case '안과':
-  case '비뇨의학과':
-  case '정신건강의학과':
-  case '재활의학과':
-  case '치과':
-    buttonstr2 = {
-      label: "컨택센터 전화예약",
-      action: "phone",
-      phoneNumber: "055-270-1000"
-    }
-    //texthelp = "해당 진료과는 전화예약만 가능합니다. 컨텍센터로 연락부탁드립니다."
-    break;
-
-  default:
-    buttonstr2 = {
-      label: "모바일예약 이동",
-      action: "webLink",
-      webLinkUrl: yelink
-    }
-    //texthelp = "진료를 원하시는 의료진을 선택해주세요."
-}
-
-
-
-const responseBody = {
-  version: "2.0",
-  template: {
-    outputs: [{
-        listCard: {
-          header: {
-            title: "창원파티마병원 의료진",
-            imageUrl: "https://www.fatimahosp.co.kr/assets/images/sub/sub_visual5.jpg"
-          },
-          items: filterbody,
-          buttons: [
-            buttonstr1,
-            buttonstr2
-          ]
-        }
-      },
-      {
-        simpleText: {
-          text: texthelp
-        }
-      }
-    ]
+  //console.log(buttonstr1)
+  buttonstr1 = JSON.parse(buttonstr1)
+  for (let i = 0; i < filterbody.length; i++) {
+    filterbody[i].title = filterbody[i].title + ' ' + filterbody[i].deptname
   }
-}
+  let drcode = filterbody[0].drcode
 
-return responseBody
+  let texthelp = '';
+  let yelink = "https://www.fatimahosp.co.kr/pages/department?drcode=" + drcode
+  //console.log(typeof filterbody, filterbody.bodydeptname)
+  //console.log('deptname = ' + deptname)
+  switch (deptname) {
+    case '피부과':
+    case '안과':
+    case '비뇨의학과':
+    case '정신건강의학과':
+    case '재활의학과':
+    case '치과':
+      buttonstr2 = {
+        label: "컨택센터 전화예약",
+        action: "phone",
+        phoneNumber: "055-270-1000"
+      }
+      //texthelp = "해당 진료과는 전화예약만 가능합니다. 컨텍센터로 연락부탁드립니다."
+      break;
+
+    default:
+      buttonstr2 = {
+        label: "모바일예약 이동",
+        action: "webLink",
+        webLinkUrl: yelink
+      }
+      //texthelp = "진료를 원하시는 의료진을 선택해주세요."
+  }
+
+
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [{
+          listCard: {
+            header: {
+              title: "창원파티마병원 의료진",
+              imageUrl: "https://www.fatimahosp.co.kr/assets/images/sub/sub_visual5.jpg"
+            },
+            items: filterbody,
+            buttons: [
+              buttonstr1,
+              buttonstr2
+            ]
+          }
+        },
+        {
+          simpleText: {
+            text: texthelp
+          }
+        }
+      ]
+    }
+  }
+
+  return responseBody
 }
